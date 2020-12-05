304f986c1d14ab0c4b0c8c284292dd09c6e1d12d
iexus <2.-@twopointline.com>
2020-12-05 22:04:58 +0000

    Refactor a little

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,40 @@ fn main() {
 
     let puzzle_lines: Vec<i32> = puzzle_input.lines().map(|x| x.parse::<i32>().unwrap()).collect();
 
-    // match find_matching_numbers(puzzle_lines) {
-    //     Some(result) => println!("Result: {}", result),
-    //     None => println!("Could not find a matching series")
-    // }
+    match find_double_matching(&puzzle_lines) {
+        Some(result) => println!("TryA simple: {}", result),
+        None => println!("Could not find a matching series TryA")
+    }
 
-    match search_with_lookup(puzzle_lines) {
-        Some(result) => println!("Result: {}", result),
-        None => println!("Could not find a matching series")
+    match find_triple_matching(&puzzle_lines) {
+        Some(result) => println!("TryB simple: {}", result),
+        None => println!("Could not find a matching series TryB")
+    }
+
+    match search_with_lookup(&puzzle_lines) {
+        Some(result) => println!("TryB lookup: {}", result),
+        None => println!("Could not find a matching series TryB Lookup")
     }
 }
 
-fn find_matching_numbers(puzzle_lines: Vec<i32>) -> Option<i32> {
+fn find_double_matching(puzzle_lines: &Vec<i32>) -> Option<i32> {
+    let size = puzzle_lines.len();
+
+    for i in 0..size {
+        let val1 = puzzle_lines.get(i).unwrap();
+
+        for j in i+1..size {
+            let val2 = puzzle_lines.get(j).unwrap();
+            if val1 + val2 == 2020 {
+                return Some(val1 * val2);
+            }
+        }
+    }
+
+    return None;
+}
+
+fn find_triple_matching(puzzle_lines: &Vec<i32>) -> Option<i32> {
     let size = puzzle_lines.len();
 
     for i in 0..size {
@@ -41,7 +63,7 @@ fn find_matching_numbers(puzzle_lines: Vec<i32>) -> Option<i32> {
     return None;
 }
 
-fn search_with_lookup(puzzle_lines: Vec<i32>) -> Option<i32> {
+fn search_with_lookup(puzzle_lines: &Vec<i32>) -> Option<i32> {
     let values = HashSet::<i32>::from_iter(puzzle_lines.iter().cloned());
 
     let size = puzzle_lines.len();
