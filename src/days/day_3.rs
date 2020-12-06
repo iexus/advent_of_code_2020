7392a9ea055cb9f11d5bc682bf9ea9e2453a0070
iexus <2.-@twopointline.com>
2020-12-06 19:03:07 +0000

    Day 3 part B - refactor to do both

--- src/days/day_3.rs
+++ src/days/day_3.rs
@@ -1,13 +1,30 @@
 pub fn call(puzzle_input: String) {
 
-    // down 1, right 3
+    let map: Vec<Vec<char>> = puzzle_input.lines().map(|x| x.chars().collect()).collect();
 
-    let lines: Vec<Vec<char>> = puzzle_input.lines().map(|x| x.chars().collect()).collect();
+    let single_slope_count = calculate_tree_to_slope_ratio(3, 1, &map);
 
+    let slopes = [
+        [1,1],
+        [5,1],
+        [7,1],
+        [1,2]
+    ];
+
+    let multi_slope_count = slopes.iter().fold(1, |acc, slope|{
+        acc * calculate_tree_to_slope_ratio(slope[0], slope[1], &map)
+    });
+
+    println!("Tree count A: {}", single_slope_count);
+    println!("Tree count B: {}", single_slope_count * multi_slope_count);
+}
+
+fn calculate_tree_to_slope_ratio(across: usize, down: usize, map: &Vec<Vec<char>>) -> usize {
     let mut current_index_right = 0;
     let mut tree_count = 0;
 
-    for line in lines {
+    let map_iterator = map.iter().step_by(down);
+    for line in map_iterator {
         let length = line.len();
 
         let position = line[current_index_right % length];
@@ -17,8 +34,8 @@ pub fn call(puzzle_input: String) {
             _ => println!("Unknown map symbol.")
         }
 
-        current_index_right += 3;
+        current_index_right += across;
     }
 
-    println!("Tree count: {}", tree_count);
+    tree_count
 }
