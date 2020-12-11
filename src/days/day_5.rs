cf65e39fcfb2c85ab71904327a705614ecf16f9f
iexus <2.-@twopointline.com>
2020-12-11 13:32:35 +0000

    Tidy up and do in a single pass

--- src/days/day_5.rs
+++ src/days/day_5.rs
@@ -1,20 +1,12 @@
 
 pub fn call(puzzle_input: String) {
-    let boarding_passes: Vec<Vec<char>> = puzzle_input.lines()
-        .map(|x| x.chars().collect())
-        .collect();
-
     let mut highest_seat_id = 0;
     let mut lowest_seat_id = 60;
     let mut sum_of_seats: u32 = 0;
 
-    for pass in boarding_passes {
-        let instructions = split_instructions(&pass);
-
-        let row_value = shift_bits_for_chars(instructions.0, &'B');
-        let col_value = shift_bits_for_chars(instructions.1, &'R');
-
-        let seat_id: u16 = (row_value * 8) + col_value;
+    puzzle_input.lines().for_each(|x| {
+        let pass: Vec<char> = x.chars().collect();
+        let seat_id = shift_bits_for_chars(&pass[..]);
 
         if seat_id > highest_seat_id {
             highest_seat_id = seat_id;
@@ -23,57 +15,41 @@ pub fn call(puzzle_input: String) {
         }
 
         sum_of_seats += seat_id as u32;
-    }
+    });
 
     // assuming contigious values with gaps at top and bottom - we can just sum and know the missing value.
-    // This expected total could be cleverer I think: n×(n+1)/2 and subtract the missing vals from top and bottom?
-    let expected_total: u32 = (lowest_seat_id..=highest_seat_id).fold(0, |acc, val| acc + val as u32);
-    let our_seat = expected_total - sum_of_seats;
+    // we can use n×(n+1)/2 to get the sum from 0..n and subtract the missing vals from the bottom 0..lowest_seat
+    let total_sum_no_missing = highest_seat_id * (highest_seat_id+1) / 2;
+    let lower_missing: u32 = (0..lowest_seat_id).sum();
+
+    let our_seat = total_sum_no_missing - lower_missing - sum_of_seats;
 
     println!("Highest seat_id: {}", highest_seat_id);
     println!("Our seat: {}", our_seat);
 }
 
-fn shift_bits_for_chars(bits: &[char], mask: &char) -> u16 {
+fn shift_bits_for_chars(bits: &[char]) -> u32 {
     let mut converted_row = 0;
     for bit in bits {
         converted_row <<= 1;
 
-        if bit == mask {
-            converted_row |= 0b0000_0001;
+        if bit == &'B' || bit == &'R' {
+            converted_row |= 0b0000_0000_0001;
         }
     }
 
     converted_row
 }
 
-fn split_instructions(pass: &Vec<char>) -> (&[char], &[char]) {
-    (&pass[..7], &pass[7..])
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
-    fn it_splits_boarding_passes_correctly() {
-        let pass = "BFFBFBBLRL".chars().collect();
-        let expected_start: Vec<char> = "BFFBFBB".chars().collect();
-        let expected_end: Vec<char> = "LRL".chars().collect();
-
-        let instructions = split_instructions(&pass);
-        assert_eq!(instructions.0, &expected_start[..]);
-        assert_eq!(instructions.1, &expected_end[..]);
-    }
 
     #[test]
     fn it_shifts_bits_based_on_the_string() {
-        let some_chars: Vec<char> = "BFFFBBF".chars().collect();
-        let row_result = shift_bits_for_chars(&some_chars[..], &'B');
-        assert_eq!(row_result, 70);
-
-        let some_other_chars: Vec<char> = "RLL".chars().collect();
-        let column_result = shift_bits_for_chars(&some_other_chars[..], &'R');
-        assert_eq!(column_result, 4);
+        let some_chars: Vec<char> = "BFFFBBFRLL".chars().collect();
+        let row_result = shift_bits_for_chars(&some_chars[..]);
+        assert_eq!(row_result, 564);
     }
 
 }
